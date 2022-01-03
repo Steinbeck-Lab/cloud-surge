184bc8a5f790127cda8f3ce5d815ece72c092c2c
CS76 <mailcs76@gmail.com>
2022-01-03 11:17:10 +0000

    Jobs, launch pad and workder code updates

--- launchpad.py
+++ launchpad.py
@@ -9,20 +9,44 @@ r = redis.Redis(
     password='')
 
 job_id = uuid.uuid4()
-
 # print(job_id)
 
-# save the run details to redis
-# input file, start time
+#  6834d916-3f9c-41b5-beda-6d4468f24685 - 6HA
+#  98937c32-3dc5-4e89-aed0-0a318e3169b3 - 8HA
 
 # r.delete('surge_jobs')
 
 mf = []
-with open("./formulae/formulaeMax5HA.txt") as f:
+with open("./formulae/formulaeMax8HA.txt") as f:
     lines = f.readlines()
     for line in lines:
-        # print(line.rstrip())
         mf.append(line.rstrip() + "_" + str(job_id))
 
+# ['C2H6_49b4ec2c-6431-42ea-975a-69033513e7fb','...]
+
+totalJobs = len(mf)
 # r.lpush('surge_jobs', *mf)
-print(r.lrange('surge_jobs', 0, 10))
+
+# pendingJobs = r.lrange('surge_jobs', 0, -1)
+# print(pendingJobs)
+
+jobId = "6834d916-3f9c-41b5-beda-6d4468f24685"
+print("Job id:" + str(jobId))
+pendingJobs = r.lrange('surge_jobs', 0, -1)
+pendingJobsCount = len(pendingJobs)
+print("Pending mfs:" + str(pendingJobsCount))
+completedJobs = r.lrange(jobId + ':completed', 0, -1)
+completedJobsCount = len(completedJobs)
+print("Completed mfs:" + str(completedJobsCount))
+failedJobs = r.lrange(jobId + ':failed', 0, -1)
+# print(failedJobs)
+failedJobsCount = len(failedJobs)
+print("Failed mfs:" + str(failedJobsCount))
+missingJobsCount = totalJobs - (completedJobsCount + failedJobsCount + pendingJobsCount)
+print("Missing mfs:" + str(missingJobsCount))
+# print(r.get((jobId+":C2Br2IH:stderr")))
+# print(r.get('dbe40224-bb77-477b-a208-9a036d7f8544:'))
+# print(r.lrange('surge_jobs', 0, -1))
+
+# for key in r.scan_iter("*"):
+#     print(key)
--- worker.py
+++ worker.py
@@ -5,6 +5,7 @@ import os
 from subprocess import Popen, PIPE
 from google.cloud import storage
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
 
 host="redis"
 bucket_name = 'steinbeck-surge-results'
@@ -15,15 +16,15 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name)
     os.remove(source_file_name)
-    print(
-        "File {} uploaded to {}.".format(
-            source_file_name, destination_blob_name
-        )
-    )
+    # print(
+    #     "File {} uploaded to {}.".format(
+    #         source_file_name, destination_blob_name
+    #     )
+    # )
 
 q = rediswq.RedisWQ(name="surge_jobs", host=host, port=6379, password='')
-print("Worker with sessionID: " +  q.sessionID())
-print("Initial queue state: empty=" + str(q.empty()))
+# print("Worker with sessionID: " +  q.sessionID())
+# print("Initial queue state: empty=" + str(q.empty()))
 while not q.empty():
   item = q.lease(lease_secs=10, block=True, timeout=2) 
   if item is not None:
@@ -31,9 +32,16 @@ while not q.empty():
     values =  itemstr.split("_")
     job = values[1]
     mf = values[0]
-    print(mf)
+    q._db.set(job + ':' + mf + ':' + 'start', str(datetime.now()))
     process = Popen(['surge', '-o'+mf+".smiles.gz", '-z', '-S',  mf], stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
-    upload_blob(bucket_name, mf+".smiles.gz", job + "/" + mf+".smiles.gz" )
-    q.complete(item)
-print("Queue empty, exiting")
+    q._db.set(job + ':' + mf + ':' + 'stdout', stdout)
+    q._db.set(job + ':' + mf + ':' + 'stderr', stderr)
+    if process.returncode != 0: 
+        q._db.lpush(job + ':' + 'failed', mf)
+        q.complete(item)
+    else:
+        q._db.set(job + ':' + mf + ':' + 'end', str(datetime.now()))
+        q._db.lpush(job + ':' + 'completed', mf)
+        upload_blob(bucket_name, mf+".smiles.gz", job + "/" + mf+".smiles.gz" )
+        q.complete(item)
