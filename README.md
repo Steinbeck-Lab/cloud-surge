e19892f75b76de6f3e373ea04d874b73f43412d7
CS76 <mailcs76@gmail.com>
2022-01-05 12:07:15 +0000

    added readme and other files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Mapping:
+```
 4d10411e-6eb6-439a-9f03-1cb9ed3479bc : 6HA ~ 20
 c74df038-d62e-40ff-af2c-b2f47cbf50a2 : 6HA (1CPU) ~ 20
 a5f66062-95b0-4611-8b58-b6d005ce1700: 8HA ~ 20
@@ -12,7 +13,37 @@ a5f66062-95b0-4611-8b58-b6d005ce1700: 8HA ~ 20
 1dc6de65-37a1-496b-a993-63e3d1414bd8: 11HA (1CPU) ~ 100 ~ 37mins
 7acadc37-0a46-4d04-a999-f0c6eea0357e: 11HA (2CPU) ~ 100 ~ 27mins ??
 a313c5f4-0b30-408b-945d-dd4b5a22a324: 12HA (1CPU) ~ 100 ~ failed (ephemeral storage limit reached 4g)
-b27dc908-a5a3-4333-bfc9-c1a56e2d443c: 12HA (1CPU) ~ 100 ~ 16g
+b27dc908-a5a3-4333-bfc9-c1a56e2d443c: 12HA (1CPU) ~ 100 ~ 16g es 
+b44c4a6d-bbaf-43e7-b23c-27ecdd6284b4: 12HA (2CPU) ~ 100 ~ 16g es (199065)
+```
+
+outputfolder: logs (mf,start time, endtime, stdout, stderr, upload start, upload end, filesize)
+
+bucket: steinbeck-surge-results
+
+```
+gsutil -m cp -r "gs://steinbeck-surge-results/<jobid>/" .
+gsutil -m cp -r "gs://steinbeck-surge-results/1dc6de65-37a1-496b-a993-63e3d1414bd8/" .
+```
+
+Investigate
+1) With the increase in number of jobs will the total time to generate structures increase or not (yes obviously)
+2) With current GKE limitations whats the maximum jobs we can deploy
+Any limitations because of our job deployment architecture
+3) Memory footprint
+4) CPU vs runtime
+5) Upload times and % of the total runtime is upload time
+6) Filesize vs HA or put it in another way total structures generated vs HA (valency dependence)
+7) estimates of total run time for 14HA, 15HA, 16HA....
+8) estimates of total ouput filesizes (compressed and total)
+9) estimates of costs on GKE for 14HA, 15HA ....
+10) cost savings on using committed compute 1year, 3years and zones based variation
+11) Estimates of costs for storage
+12) Final output for 14HA and host it publicly for anyone to download (cost estimates to do this for next few years)
+13) Estimates for 18HA generation / filesizes / hosting
+
+
+**Commands**
 
 Redis:
 
@@ -47,6 +78,8 @@ kubectl delete jobs `kubectl get jobs -o custom-columns=:.metadata.name`
 kubectl get pods --all-namespaces | grep Evicted | awk '{print $2, "--namespace", $1}' | xargs kubectl delete pod
 
 kubectl get svc --all-namespaces -o json | jq '.items[] | {name:.metadata.name, ns:.metadata.namespace, p:.spec.ports[] } | select( .p.nodePort != null ) | "\(.ns)/\(.name): localhost:\(.p.nodePort) -> \(.p.port) -> \(.p.targetPort)"'
+
+**Links**
 
 https://kubernetes.io/docs/reference/kubectl/cheatsheet/
 
--- launchpad.py
+++ launchpad.py
@@ -15,72 +15,72 @@ def chunks(lst, n):
 
 # r.delete('surge_jobs')
 
-# job_id = uuid.uuid4()
-# # print(job_id)
+job_id = uuid.uuid4()
+# print(job_id)
 
-# mf = []
-# with open("./formulae/formulaeMax12HA.txt") as f:
-#     lines = f.readlines()
-#     for line in lines:
-#         mf.append(line.rstrip() + "_" + str(job_id))
-
-# totalJobs = len(mf)
-# # r.lpush('surge_jobs', *mf)
-
-# # pendingJobs = r.lrange('surge_jobs', 0, -1)
-# # print(len(pendingJobs))
-
-# jobId = "b27dc908-a5a3-4333-bfc9-c1a56e2d443c"
-# print("Job id:" + str(jobId))
-# pendingJobs = r.lrange('surge_jobs', 0, -1)
-# pendingJobsCount = len(pendingJobs)
-# print("Pending mfs:" + str(pendingJobsCount))
-# completedJobs = r.lrange(jobId + ':completed', 0, -1)
-# completedJobsCount = len(completedJobs)
-# print("Completed mfs:" + str(completedJobsCount))
-# failedJobs = r.lrange(jobId + ':failed', 0, -1)
-# # print(failedJobs)
-# failedJobsCount = len(failedJobs)
-# print("Failed mfs:" + str(failedJobsCount))
-# processingJobsCount = totalJobs - (completedJobsCount + failedJobsCount + pendingJobsCount)
-# print("Lease / processing mfs:" + str(processingJobsCount))
-
-# Export output
-parsedKeys=[]
-exportJobId = str('b27dc908-a5a3-4333-bfc9-c1a56e2d443c')
-r.delete(exportJobId + ':failed')
-r.delete(exportJobId + ':completed')
-jobOutputFile = open("logs/" + exportJobId + '.csv',"a+")
+mf = []
 with open("./formulae/formulaeMax12HA.txt") as f:
     lines = f.readlines()
-    nkey = exportJobId
-    for mfs in chunks(lines, 500):
-        smfs = []
-        for mf in mfs:
-            if mf not in parsedKeys:
-                smfs.append(nkey + ":" + mf.rstrip())
-        mfdata = r.mget(smfs)
-        for mfd in mfdata:
-            mObj = {}
-            if mfd:
-                value = json.loads(mfd.decode("utf-8"))
-                sout = value['stdErr'].rstrip().replace('\n', '|').replace('\r', '|')
-                mObj['mf'] = sout.split("  ")[0]
-                mObj['start'] = value['start']
-                mObj['end'] = value['end']
-                mObj['stdOut'] = value['stdOut'].rstrip().replace('\n', '|').replace('\r', '|')
-                mObj['stdErr'] = sout
-                mObj['uploadStart'] = value['uploadStart']
-                mObj['uploadEnd'] = value['uploadEnd']
-                mObj['oFileSize'] = str(value['oFileSize'])
-                jobOutputFile.write(",".join(list(mObj.values()))+'\n')
-                # r.delete(key)
-                parsedKeys.append(nkey)
-            else:
-                print(mfd)
-        print(len(parsedKeys))
-# delete parsed keys
-jobOutputFile.close()
+    for line in lines:
+        mf.append(line.rstrip() + "_" + str(job_id))
+
+totalJobs = len(mf)
+# r.lpush('surge_jobs', *mf)
+
+# pendingJobs = r.lrange('surge_jobs', 0, -1)
+# print(len(pendingJobs))
+
+jobId = "b44c4a6d-bbaf-43e7-b23c-27ecdd6284b4"
+print("Job id:" + str(jobId))
+pendingJobs = r.lrange('surge_jobs', 0, -1)
+pendingJobsCount = len(pendingJobs)
+print("Pending mfs:" + str(pendingJobsCount))
+completedJobs = r.lrange(jobId + ':completed', 0, -1)
+completedJobsCount = len(completedJobs)
+print("Completed mfs:" + str(completedJobsCount))
+failedJobs = r.lrange(jobId + ':failed', 0, -1)
+# print(failedJobs)
+failedJobsCount = len(failedJobs)
+print("Failed mfs:" + str(failedJobsCount))
+processingJobsCount = totalJobs - (completedJobsCount + failedJobsCount + pendingJobsCount)
+print("Lease / processing mfs:" + str(processingJobsCount))
+
+# # Export output
+# parsedKeys=[]
+# exportJobId = str('b27dc908-a5a3-4333-bfc9-c1a56e2d443c')
+# r.delete(exportJobId + ':failed')
+# r.delete(exportJobId + ':completed')
+# jobOutputFile = open("logs/" + exportJobId + '.csv',"a+")
+# with open("./formulae/formulaeMax12HA.txt") as f:
+#     lines = f.readlines()
+#     nkey = exportJobId
+#     for mfs in chunks(lines, 500):
+#         smfs = []
+#         for mf in mfs:
+#             if mf not in parsedKeys:
+#                 smfs.append(nkey + ":" + mf.rstrip())
+#         mfdata = r.mget(smfs)
+#         for mfd in mfdata:
+#             mObj = {}
+#             if mfd:
+#                 value = json.loads(mfd.decode("utf-8"))
+#                 sout = value['stdErr'].rstrip().replace('\n', '|').replace('\r', '|')
+#                 mObj['mf'] = sout.split("  ")[0]
+#                 mObj['start'] = value['start']
+#                 mObj['end'] = value['end']
+#                 mObj['stdOut'] = value['stdOut'].rstrip().replace('\n', '|').replace('\r', '|')
+#                 mObj['stdErr'] = sout
+#                 mObj['uploadStart'] = value['uploadStart']
+#                 mObj['uploadEnd'] = value['uploadEnd']
+#                 mObj['oFileSize'] = str(value['oFileSize'])
+#                 jobOutputFile.write(",".join(list(mObj.values()))+'\n')
+#                 # r.delete(key)
+#                 parsedKeys.append(nkey)
+#             else:
+#                 print(mfd)
+#         print(len(parsedKeys))
+# # delete parsed keys
+# jobOutputFile.close()
 
 # Delete all keys
 # for key in r.scan_iter("*"):
