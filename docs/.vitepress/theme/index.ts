8f83cd7ce8a66d598942ada17bc1bdba01cfd2fc
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-10-04 19:32:24 +0200

    docs: implemented image zoom

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,9 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './style.css'
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
 
 export default {
   extends: Theme,
@@ -12,5 +15,19 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
+  },
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); 
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
 }
